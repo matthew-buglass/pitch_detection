73654a921b3a834d938cf6f363924ce2e4ab05fd
mab839 <mab839@usask.ca>
2021-10-06 18:28:15 -0600

    Initial Commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#   Copyright (c) Matthew Buglass
+#   Author: Matthew Buglass
+#   Maintainer: Matthew Buglass
+#   Website: matthewbuglass.com
+#   Date: 2021/10/6
+
+# implementation of an FFT (fast-fourier-transform) to do pitch detection of live audio
+
+
+
